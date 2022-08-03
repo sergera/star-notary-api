0255e8dadb7cd897f0c42233ecd8b1ddbc23cbd1
sergera <sergera88@gmail.com>
2022-08-03 19:12:00 -0300

    Database Duplicate Entry Fix
    - Fixed database accepting same wallet address or name as different
    entries because of capitalization

--- create_tables.sql
+++ create_tables.sql
@@ -1,20 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS CITEXT;
+
 CREATE TABLE IF NOT EXISTS wallets (
   id SERIAL PRIMARY KEY,
-	address CHAR(42) NOT NULL,
-	CONSTRAINT wallet_address UNIQUE (address)
+	address CITEXT NOT NULL,
+	CONSTRAINT wallet_address UNIQUE (address),
+	CONSTRAINT wallet_address_length CHECK (LENGTH(address) = 42)
 );
 
 CREATE INDEX wallet_address_idx ON wallets(address);
 
 CREATE TABLE IF NOT EXISTS stars (
 	id BIGINT PRIMARY KEY,
-	name VARCHAR(32) NOT NULL,
+	name CITEXT NOT NULL,
 	coordinates CHAR(19) UNIQUE NOT NULL,
 	is_for_sale BOOLEAN NOT NULL,
 	price_ether NUMERIC(30,18),
 	date_created TIMESTAMP NOT NULL,
 	owner_id INTEGER REFERENCES wallets(id) NOT NULL,
 	CONSTRAINT star_name UNIQUE (name),
+	CONSTRAINT star_name_length CHECK (LENGTH(name) >= 4 AND LENGTH(name) <= 32),
 	CONSTRAINT star_coordinates UNIQUE (coordinates)
 );
 
@@ -31,7 +35,9 @@ CREATE TABLE IF NOT EXISTS sales_history (
 
 CREATE TABLE IF NOT EXISTS names_history (
 	date_set TIMESTAMP NOT NULL,
-	name VARCHAR(32) NOT NULL,
+	name CITEXT NOT NULL,
 	star_id BIGINT REFERENCES stars(id) NOT NULL,
-	owner_id INTEGER REFERENCES wallets(id) NOT NULL
+	owner_id INTEGER REFERENCES wallets(id) NOT NULL,
+	CONSTRAINT star_name_history UNIQUE (name),
+	CONSTRAINT star_name_history_length CHECK (LENGTH(name) >= 4 AND LENGTH(name) <= 32)
 );
