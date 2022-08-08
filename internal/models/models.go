01665b4920cefed415469c78ccb5afca096bad92
sergera <sergera88@gmail.com>
2022-08-08 15:47:28 -0300

    Model Marshalled JSON Field Name Change
    - Marshalled JSON field names now follow the snake case pattern

--- internal/models/models.go
+++ internal/models/models.go
@@ -8,12 +8,12 @@ import (
 )
 
 type StarModel struct {
-	TokenId     string    `json:"tokenid"`
+	TokenId     string    `json:"token_id"`
 	Owner       string    `json:"owner"`
 	Coordinates string    `json:"coordinates"`
 	Name        string    `json:"name"`
 	Price       string    `json:"price"`
-	IsForSale   bool      `json:"isforsale"`
+	IsForSale   bool      `json:"is_for_sale"`
 	Date        time.Time `json:"date"`
 }
 
